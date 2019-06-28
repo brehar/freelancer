f3e3f3736fed8bd7f328104383d5e9f3741d2271
Brett Hartman <brett.hartman@mac.com>
2019-06-28 10:48:30 -0700

    Create Users controller with dashboard action and implement routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def dashboard
+  end
+
+  def update
+    @user = current_user
+
+    if @user.update_attributes(current_user_params)
+      flash[:notice] = 'Your changes have been saved successfully.'
+    else
+      flash[:alert] = 'There has been an error applying your changes.'
+    end
+
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def current_user_params
+    params.require(:user).permit(:from, :about, :status, :language)
+  end
+end
