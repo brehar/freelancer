eb433cf430a450f12d91c8c00fc02b9f745715ca
Brett Hartman <brett.hartman@mac.com>
2019-06-28 10:21:52 -0700

    Implement Facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
 
-  validates :full_name, presence: true, length: { maximum: 50 }
+  validates :full_name, presence: true, length: {maximum: 50}
+
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+
+    return user if user
+
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.full_name = auth.info.name
+      user.image = auth.info.image
+      user.uid = auth.uid
+      user.provider = auth.provider
+    end
+  end
 end
