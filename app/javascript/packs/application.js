c41bc5d960da92930c39e1055620de1bf99d7465
Brett Hartman <brett.hartman@mac.com>
2019-06-29 10:52:03 -0700

    Create dashboard page UI with functionality to update user profile information

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,11 @@ require('@rails/activestorage').start();
 require('channels');
 
 window.Noty = require('noty');
+
+$(document).ready(() => {
+	$('.toggle').on('click', e => {
+		e.stopPropagation();
+		e.preventDefault();
+		$('#' + e.target.getAttribute('aria-controls')).toggleClass('is-hidden');
+	});
+});
